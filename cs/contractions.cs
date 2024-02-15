352273aa60c4809aee17e71ddebd6662eb536fb5
Goomba <84668888+GoombaProgrammer@users.noreply.github.com>
2024-02-15 09:05:39 +0100

    Yay

--- cs/contractions.cs
+++ cs/contractions.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using Newtonsoft.Json;
@@ -8,19 +9,19 @@ namespace Kootenpv.Contractions
 {
     public class Contractions
     {
-        static Dictionary<string, string> contractions = new Dictionary<string, string>();
-        static Dictionary<string, string> leftovers = new Dictionary<string, string>();
-        static Dictionary<string, string> slang = new Dictionary<string, string>();
-        public static void Convert(string text)
+        public static Dictionary<string, string> contractions = new Dictionary<string, string>();
+        public static Dictionary<string, string> leftovers = new Dictionary<string, string>();
+        public static Dictionary<string, string> slang = new Dictionary<string, string>();
+        public static string Convert(string text)
         {
-            contraction = JsonConvert.DeserializeObject<Dictionary<string, string>>(System.IO.File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "data", "contractions_dict.json")));
+            contractions = JsonConvert.DeserializeObject<Dictionary<string, string>>(System.IO.File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "data", "contractions_dict.json")));
             leftovers = JsonConvert.DeserializeObject<Dictionary<string, string>>(System.IO.File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "data", "leftovers_dict.json")));
             slang = JsonConvert.DeserializeObject<Dictionary<string, string>>(System.IO.File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "data", "slang_dict.json")));
 
             string[] months = new string[]
             {
                 "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"
-            }
+            };
 
             for (int i = 0; i < months.Length; i++)
             {
